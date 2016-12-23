bbe41ae039ecbc8dc1a30615add2ccad39b4f009
poojachatiri <poojachathiri@gmail.com>
2016-12-23 15:40:15 +0530

    Add files via upload

---
+++ de.py
@@ -0,0 +1,11 @@
+n =int(raw_input())
+if n % 2:
+   print ("odd")
+else:
+   if n<5:
+      print ("between 2 &5")
+   else:
+       if (n>=6 and n<=20):
+          print ("6 to 20")
+       else:
+          print ("20more")
